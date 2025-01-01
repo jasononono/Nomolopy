f412a0d2a114e425fe44bc1785404cfb0ba5a0d2
Richard Zhang <zhanr0572@wrdsb.ca>
2025-01-01 09:13:08 -0500

    Imported files

---
+++ gui.py
@@ -0,0 +1,230 @@
+#################### IMPORT ####################
+
+
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import sys
+
+#################### COLOUR PALETTE ####################
+
+
+BLUE0 = '#ceddf5'
+BLUE1 = '#99baf0'
+BLUE2 = '#4e8ef5'
+WHITE = '#ffffff'
+
+# tile colours
+BROWN = '#733a14'
+BLUE = '#11cfff'
+PINK = '#dd00ff'
+ORANGE = '#ff9100'
+RED = '#ff3c00'
+YELLOW = '#ffd500'
+GREEN = '#00ba35'
+RICH = '#003eba'
+BAD = '#660103'
+
+tiles_colour = [WHITE, BROWN, WHITE, BROWN, BAD, WHITE, BLUE, WHITE, BLUE, BLUE, WHITE,
+                PINK, WHITE, PINK, PINK, WHITE, ORANGE, WHITE, ORANGE, ORANGE, WHITE,
+                RED, WHITE, RED, RED, WHITE, YELLOW, YELLOW, WHITE, YELLOW, BAD,
+                GREEN, GREEN, WHITE, GREEN, WHITE, WHITE, RICH, BAD, RICH, BAD]
+tiles_name = ["GO\n->", "Arctic\nAvenue", "Commu\n-nity\nChest", "North\nSea\nAvenue", "Income\nTax",
+              "Writing\nRail\n-road", "Disori\n-ented\nAvenue", "Chance", "Ventnor\nAvenue", "Connect\nFour\nAvenue",
+              "Just\nVisiting", "St. Nicholas\nPlace", "Electric\nCompany", "Provinces\nAvenue", "Washington\nAvenue",
+              "Pencil\nRailroad", "Crane's\nPlace", "Community\nChest", "Tessinee\nAvenue", "Old York\nAvenue",
+              "Free\nParking", "Chic\n-ken\nAvenue", "Chance", "India\nAvenue", "Healthi\n-nois\nAvenue",
+              "M&M\nRail\n-road", "Pacific\nAvenue", "Ver\n-mont\nAvenue", "Water\nWorks", "Martin\nGar\n-dens",
+              "Go To\nJail", "Atlantic\nAvenue", "South Caro\n-lina Avenue", "Community\nChest", "Pencil\nAvenue",
+              "Long Line", "Chance", "Drive Place", "Luxury Tax", "Plankrun", "Jail"]
+LOCATION = 'menu'
+
+#################### FUNCTIONS ####################
+
+
+def importFile(p):
+    global players_tokenImg
+    path = filedialog.askopenfilename(title = 'select token', filetypes = [("Png Files", "*.png")])
+    if path:
+        img = Image.open(rf'{path}')
+        img = img.resize([100, 100])
+        img = ImageTk.PhotoImage(img)
+
+def retrievePlayers():
+    global players_name
+    p = []
+    for i in players_name:
+        if i.get() != '':
+            p.append(i.get())
+    if len(p) > 1:
+        players_name = p
+        return True
+    return False
+
+def updateWindow(location):
+    global scr, title, subtitle, m_play
+    global players_label, players_tokenImg, players_name, players_openfile, players_token
+    global tiles_bd, tiles_obj, tiles_frame, tiles_label, mainDisplay
+
+    if location == 'board' and not retrievePlayers():
+        return
+
+    title.place_forget()
+    subtitle.place_forget()
+    m_play.place_forget()
+
+    for i in range(5):
+        players_label[i].grid_forget()
+        players_openfile[i].grid_forget()
+
+    s_play.place_forget()
+
+    for i in range(41):
+        tiles_bd[i].place_forget()
+        tiles_obj[i].place_forget()
+        tiles_frame[i].place_forget()
+        tiles_label[i].pack_forget()
+    mainDisplay.place_forget()
+
+    if location == 'menu':
+        scr.config(bg=BLUE1)
+
+        title.place(anchor=CENTER, x=400, y=300)
+        subtitle.place(anchor=CENTER, x=400, y=400)
+        m_play.place(anchor=CENTER, x=400, y=500)
+
+    if location == 'setup':
+        for i in range(5):
+            players_label[i].grid(row=i, column=0)
+            players_openfile[i].grid(row = i, column = 1)
+
+        s_play.place(anchor=CENTER, x=400, y=500)
+
+    if location == 'board':
+        scr.config(bg=BLUE0)
+        players_token = []
+        for i in range(len(players_name)):
+            img = transitionImg
+            if players_tokenImg[i] != '':
+                img = players_tokenImg[i]
+            players_token.append(Label(scr, image = img))
+            players_token[-1].place(anchor = CENTER, x = 400, y = 400)
+        title.place(anchor=CENTER, x=400, y=380)
+        subtitle.place(anchor=CENTER, x=400, y=450)
+        for i in range(40):
+            mod, rem = i % 10, i // 10
+            if mod == 0:
+                pos = [52, 52, 748, 748, 52]
+            else:
+                pos = [52, 66 * mod + 70, 748, 730 - 66 * mod, 52]
+            pos = [pos[rem + 1], pos[rem]]
+
+            tiles_bd[i].place(anchor=CENTER, x=pos[0], y=pos[1])
+            tiles_obj[i].place(anchor=CENTER, x=pos[0], y=pos[1])
+            tiles_frame[i].place(anchor=CENTER, x=pos[0], y=pos[1])
+            tiles_label[i].pack()
+        tiles_bd[-1].place(anchor=CENTER, x=600, y=200)
+        tiles_obj[-1].place(anchor=CENTER, x=600, y=200)
+        tiles_frame[-1].place(anchor=CENTER, x=600, y=200)
+        tiles_label[-1].pack()
+
+        mainDisplay.place(anchor=CENTER, x=400, y=520)
+
+def updateAnimation(location, direction):
+    global LOCATION
+
+    transition.place(x=800 * direction, y=0)
+    for i in range(15):
+        current = int(transition.place_info().get('x'))
+        spd = (15 - (255 - i ** 2) ** 0.5) * direction
+        transition.place(x=current - 20 * spd, y=0)
+        scr.update()
+    updateWindow(location)
+    for i in range(15, 30):
+        current = int(transition.place_info().get('x'))
+        spd = (15 - (i * 60 - i ** 2 - 675) ** 0.5) * direction
+        transition.place(x=current - 20 * spd, y=0)
+        scr.update()
+    LOCATION = location
+
+
+def moveToken(token, start, end):
+    return
+
+def getPlayers():
+    return len(players_name), players_name
+
+
+#################### TK ####################
+
+
+scr = Tk()
+scr.geometry('800x800')
+scr.title(' NOMOLOPY ')
+#################### MAIN MENU ####################
+
+
+### title & subtitle ###
+
+title = Label(scr, bg=BLUE0, fg=BLUE1, text='NOMOLOPY', font='arial 80 bold')
+subtitle = Label(scr, bg=BLUE0, fg=BLUE1, text='~ Very Original, est. 2024 ~', font='optima 40 italic')
+
+### buttons ###
+
+m_play = Button(scr, bg=BLUE1, fg=BLUE2, text='PLAY', font='aharoni 60', command=lambda: updateAnimation('setup', 1))
+
+#################### SETUP MENU ####################
+
+
+players_name = [StringVar(scr, '') for i in range(5)]
+players_label = []
+players_openfile = []
+players_tokenImg = [''] * 5
+players_token = []
+for i in range(5):
+    players_label.append(Entry(scr, width=10, textvariable=players_name[i], font='optima 50 italic bold'))
+    players_openfile.append(Button(scr, text = 'Token', command = lambda x = i: importFile(x)))
+
+s_play = Button(scr, bg=BLUE1, fg=BLUE2, text='PLAY', font='aharoni 60', command=lambda: updateAnimation('board', 1))
+
+#################### GAME BOARD ####################
+
+
+### tiles ###
+
+tiles_bd = []
+tiles_obj = []
+tiles_frame = []
+tiles_label = []
+for i in range(40):
+    size = [100] * 2
+    if i % 10:
+        if (i // 10) % 2:
+            size[1] = 64
+        else:
+            size[0] = 64
+    tiles_bd.append(Frame(scr, bg=tiles_colour[i], width=size[0], height=size[1]))
+    tiles_obj.append(Frame(scr, bg=BLUE1, width=size[0] - 4, height=size[1] - 4))
+    tiles_frame.append(Frame(scr, bg=BLUE1, width=size[0] - 4, height=size[1] - 4))
+    tiles_label.append(Label(tiles_frame[-1], bg=BLUE1, fg=tiles_colour[i], text=tiles_name[i], font='aharoni 25 bold'))
+    if i % 10:
+        tiles_label[-1].config(font='aharoni 15')
+
+tiles_bd.append(Frame(scr, bg=BAD, width=150, height=150))
+tiles_obj.append(Frame(scr, bg=BLUE1, width=146, height=146))
+tiles_frame.append(Frame(scr, bg=BLUE1, width=146, height=146))
+tiles_label.append(Label(tiles_frame[-1], bg=BLUE1, fg=tiles_colour[-1], text=tiles_name[-1], font='aharoni 30 bold'))
+
+### dialogue ###
+
+mainDisplay = Label(scr, bg=BLUE0, fg=BLUE2, text='Crane has been fined $9127436802!!!!\noops', font='optima 30')
+
+#################### OTHER STUFF ####################
+
+
+transitionImg = Image.open('transition.png')
+transitionImg = transitionImg.resize([800, 800])
+transitionImg = ImageTk.PhotoImage(transitionImg)
+transition = Label(scr, image=transitionImg, bd=0)
+
+#################### MAINLOOP ####################
---
+++ playerModule.py
@@ -0,0 +1,219 @@
+import random
+from data import *
+
+community_chest_spaces = (2, 17, 33)
+chance_spaces = (7, 22, 36)
+railroads = (5, 15, 25, 35)
+utilities = (12, 28)
+null_space = (0, 10, 20)
+
+
+class Player:
+
+    def __init__(self, name):
+        self.player_name = name
+        self.position = 0
+        self.money = 1500
+        self.owned_properties = []
+        self.has_set = False
+        self.sets = []
+        self.loop = True
+
+    def rollDice(self, past_roll, double_count):
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print(f"Rolled {dice1} and {dice2}")
+        roll = past_roll + dice1 + dice2
+        print("The roll is now " + str(roll))
+        if dice1 == dice2:
+            if self.position == 40:
+                self.position = 10
+                return roll
+            else:
+                double_count += 1
+                if double_count >= 3:
+                    print("You rolled 3 doubles in a row. Go to jail.")
+                    self.goToJail()
+                    return None
+                else:
+                    return self.rollDice(roll, double_count)
+        elif self.position == 40:
+            print("Stay in jail")
+            return None
+        print("Rolled")
+        return roll
+
+    def move(self, num):
+        self.position = (self.position + num) % 40
+        if self.position - num < 0:
+            self.money += 200
+            print("You passed go. Collect $200.")
+
+    def goToJail(self):
+        self.position = 40
+        # UI import
+        if input("Do you want to pay $50 to get out of jail? (y/n) ").lower() == "y":  # placeholder
+            if self.money >= 50:
+                self.money -= 50
+            else:
+                if self.mortgageOrSell(50) == 1:
+                    self.position = 10
+
+    def mortgageOrSell(self, amount):
+        #UI import
+        str = ""
+        for property in self.owned_properties:
+            str += f"{property_name.index(property)}. {property_name[property]} - {property_state[property]} houses\n"
+        print(str)
+        if input("You don't have enough money to buy this. Would you like to mortgage your properties? (y/n) ").lower() == "n":
+            return -1
+        while True:
+            mortgage = self.owned_properties[int(input("Which properties would you like to mortgage?")) - 1]
+            if property_state[mortgage] > 0:
+                if mortgage < 10:
+                    self.money += 25
+                elif mortgage < 20:
+                    self.money += 50
+                elif mortgage < 30:
+                    self.money += 75
+                else:
+                    self.money += 100
+                print(f"You sold a building and now have ${self.money}.")
+            else:
+                self.money += property_purchase_price[mortgage]/2
+                print(f"You sold {property_name[mortgage]} and now have ${self.money}.")
+            print(f"The amount required is ${amount}.")
+            property_state[mortgage] -= 1
+            if self.money >= amount:
+                self.money -= amount
+                return 1
+            if len(self.owned_properties) < 1:
+                return -1
+            if input("Do you want to continue selling properties? (y/n) ").lower() == "n":
+                return -1
+
+
+
+
+    def spaceAction(self, space, players):
+        global property_name, property_owner, property_state, property_rent, property_purchase_price
+        if space == 30:
+            self.goToJail()
+        elif space in community_chest_spaces:
+            self.drawCommunityChest()
+        elif space in chance_spaces:
+            self.drawChance()
+        # Taxes
+        elif space == 4:
+            self.money -= 200
+        elif space == 38:
+            self.money -= 100
+        elif space in null_space or space in self.owned_properties:
+            pass
+        elif property_owner[space] != -1:
+            if space in railroads:
+                railroad_count = 0
+                for i in range(4):
+                    if (5 + 10 * i) in players[property_owner[space]].owned_properties:
+                        railroad_count += 1
+                rent = property_rent[space][railroad_count - 1]
+            elif space in utilities:
+                utility_count = 0
+                for space in utilities:
+                    if space in players[property_owner[space]].owned_properties:
+                        utility_count += 1
+                if utility_count == 1:
+                    rent = 4 * (random.randint(1, 6) + random.randint(1, 6))
+                else:
+                    rent = 10 * (random.randint(1, 6) + random.randint(1, 6))
+            else:
+                rent = property_rent[space][property_state[space]]
+                players[property_owner[space]].money += rent
+            if self.money >= rent:
+                self.money -= rent
+                print(f"You paid {str(rent)} to {players[property_owner[space]].player_name}.")
+            else:
+                if self.mortgageOrSell(rent - self.money) == -1:
+                    self.bankrupt()
+                else:
+                    self.money -= rent
+                    print(f"You paid {str(rent)} to {property_name[property_owner[space]]}.")
+        else:
+            price = property_purchase_price[space]
+            query = input(
+                f"Would you like to buy {property_name[space]} for ${price}? You currently have ${self.money} (y/n) ")
+            if query.lower() == 'y':
+                if self.money >= price:
+                    self.money -= price
+                else:
+                    if self.mortgageOrSell(price) == 1:
+                        self.money -= price
+                    else:
+                        print("You don't have enough money to buy this property.")
+                        return
+                print(f"You have bought this space for {price}. You now have ${self.money}.")
+                property_owner[space] = players.index(self)
+                print(f"Property has been transferred to player {players.index(self)}")
+                property_state[space] = 0
+                self.owned_properties.append(space)
+
+    def drawChance(self):
+        print("GAMBLINGGG!!!")
+        pass
+
+    def drawCommunityChest(self):
+        print("Chest")
+        pass
+
+    def purchaseBuildings(self):
+        global property_state, color_sets
+        if not self.loop:
+            return
+        if not self.has_set:
+            for set in color_sets:
+                if set in self.owned_properties:
+                    self.has_set = True
+                    self.sets.append(set)
+        if not self.has_set:
+            return
+        # UI import
+        print("You have the following sets:")
+        for set in self.sets:
+            print(set)
+        purchase_bool = input("Do you want to purchase a building? (y/n) ")  # placeholder
+        if purchase_bool.lower == "y":
+            # UI import
+            purchase_location = int(input("Which property do you want to build on? "))  # placeholder
+            if property_state[purchase_location] == 5:
+                # UI import - cannot buy more houses
+                return
+            elif purchase_location < 10:
+                cost = 50
+            elif purchase_location < 20:
+                cost = 100
+            elif purchase_location < 30:
+                cost = 150
+            else:
+                cost = 200
+            if self.money >= cost:
+                self.money -= cost
+                property_state[purchase_location] += 1
+            else:
+                if self.mortgageOrSell(cost - self.money) == 1:
+                    self.money -= cost
+                    property_state[purchase_location] += 1
+            # UI Import
+            if input("Do you want to continue purchasing buildings? (y/n)").lower == 'y':  # placeholder
+                self.purchaseBuildings()
+            else:
+                return
+        else:
+            return
+
+    def bankrupt(self):
+        global players, property_state
+        players[players.index(self)] = None
+        for property in self.owned_properties:
+            property_state[property] = -2
+            property_state[property] = -1
+        self.loop = False
